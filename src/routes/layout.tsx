ed6470e5c834004e8e1bdf5a0e24ddca80c2d030
tthugy <supermanace7@gmail.com>
2023-02-01 23:35:46 +0900

    update

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,4 @@
 import { component$, Slot } from '@builder.io/qwik';
-import { createTheme, style } from '@vanilla-extract/css';
 import Header from '../components/header/header';
 
 export default component$(() => {
